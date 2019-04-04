2a81aeca7513161cd0ddfa7ac0d562d3d70175b5
chenvvenxin <chenvvenxin@outlook.com>
2019-04-04 16:23:09 +0800

    Add files via upload

---
+++ word-segment/WSS_compile.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  3 15:13:30 2019
+
+@author: lenovo
+"""
+
+import requests
+import tkinter as tk
+from html.parser import HTMLParser
+import tkinter.font as tkFont
+
+def getResponse():
+    entry2.config(state=tk.NORMAL)
+    entry2.delete(0, tk.END)
+    ipt = str(entry1.get())
+    response = requests.get('http://word-segment.sai.corp:8090/v1/api/seg?q=' + ipt)
+    rt = eval(response.text)
+    res = ''
+    l = len(rt)
+    for i in range(l):
+        tmp = rt[i]['text']
+        res += tmp
+        if i < l-1:
+            res += '/'
+        else:
+            continue
+    entry2.insert(10, res)
+    entry2.config(state=tk.DISABLED)
+    f = open('temp.txt', mode='a+', encoding='GBK')
+    f.write('query:' + ipt + '\tanswer:' + res +'\n')
+    f.close
+
+top = tk.Tk()
+top.title('Word-Segmentation System')
+top.geometry('600x300')  #窗口尺寸
+tk.Label(top, text = 'query').place(x=180,y=110)
+tk.Label(top, text = 'result').place(x=180,y=170)
+
+entry1 = tk.Entry(top)
+entry2 = tk.Entry(top)
+entry1.place(x=250,y=110)
+entry2.place(x=250,y=170)
+
+ft = tkFont.Font(family='Fixdsys', size=22, weight=tkFont.BOLD, slant=tkFont.ITALIC)
+tk.Label(top, text='Soundai', font=ft, fg='blue').place(x=240,y=40)
+#显示图片部分
+#load = Image.open('soundai.jpg')
+#re = ImageTk.PhotoImage(load)
+#img = tk.Label(image=re)
+#img.image = re
+#img.place(x=0,y=0)
+
+a = tk.Button(top, text = 'run',width=13, height=1, command = getResponse).place(x=260,y=230)
+entry1.bind("<Return>", getResponse)
+top.mainloop()
